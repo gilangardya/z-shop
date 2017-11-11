59f3c1408cdb84fb62dcdd665ca6e67c331ec48b
gilang20 <gilangardya@yahoo.com>
2017-11-12 08:34:44 +1400

    coba di GitHub

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                 {
                     // send same message as reply to user
                     // $result = $bot->replyText($event['replyToken'], $event['message']['text']);
-                    $result = $bot->replyText($event['replyToken'], 'pesan balasan');
+                    $result = $bot->replyText($event['replyToken'], 'Ini pesan balasan lewat GitHub');
      
                     // or we can use replyMessage() instead to send reply message
                     // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
